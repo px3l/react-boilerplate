69957951a6fd323e3cc70e89fbe8eb68bc60b4ec
px3l <px3l@tuta.io>
2016-12-02 12:10:24 +0000

    cleaned

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react'
+import Graph from 'graph'
 
 class HomeComponent extends Component {
   render() {
     return (
       <div className="home">
         i am home
+        <Graph />
       </div>
     )
   }
